7360f705a5594846d731edc50d561caa51785b40
Filip Krikava <krikava@gmail.com>
2014-06-27 19:04:24 +0200

    Cleaned up single-node devstack.sh

--- single-node/devstack.sh
+++ single-node/devstack.sh
@@ -21,10 +21,18 @@ then
   git clone -b $OPENSTACK_VERSION https://github.com/openstack-dev/devstack.git "$devstack_home"
 fi
 
-cat > "$devstack_home/local.conf" <<EOF
+cd "$devstack_home"
+
+# WORKAROUND: https://bugs.launchpad.net/python-openstackclient/+bug/1326811
+patch_1326811="bug_1326811.patch" 
+if [ ! -f "$patch_1326811" ]; then
+  git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/63/98263/1 && git format-patch -1 --stdout FETCH_HEAD > "$patch_1326811"
+  git apply < "$patch_1326811"
+fi
+
+cat > "local.conf" <<EOF
 [[local|localrc]]
 
-# MULTI_HOST=1
 DEST=/opt/stack
 SCREEN_LOGDIR=/opt/stack/logs/screen
 
@@ -39,29 +47,8 @@ FLAT_INTERFACE=eth1
 FIXED_RANGE=10.1.1.0/24
 FIXED_NETWORK_SIZE=256
 FLOATING_RANGE=192.168.1.128/25
-#PUBLIC_INTERFACE=eth1
-
-# Disable default enabled services
-#disable_service cinder c-sch c-api c-vol
-#disable_service tempest
-#disable_service horizon
-
-#CINDER_BRANCH=$OPENSTACK_VERSION
-#GLANCE_BRANCH=$OPENSTACK_VERSION
-#HORIZON_BRANCH=$OPENSTACK_VERSION
-#KEYSTONE_BRANCH=$OPENSTACK_VERSION
-#NOVA_BRANCH=$OPENSTACK_VERSION
-#SWIFT_BRANCH=$OPENSTACK_VERSION
 EOF
 
-cd "$devstack_home"
-
-# WORKAROUND: https://bugs.launchpad.net/python-openstackclient/+bug/1326811
-patch_1326811="bug_1326811.patch" 
-if [ ! -f "$patch_1326811" ]; then
-  git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/63/98263/1 && git format-patch -1 --stdout FETCH_HEAD > "$patch_1326811"
-  git apply < "$patch_1326811"
-fi
 
 ./unstack.sh && FORCE=yes ./stack.sh
 
